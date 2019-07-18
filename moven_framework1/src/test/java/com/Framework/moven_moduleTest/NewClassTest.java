d61244210ad4dc055f02a09b81e5a67224676f19
Ravi <Ravi@Ravi-PC>
2019-07-18 13:07:40 +0530

    new file

---
+++ moven_framework1/src/test/java/com/Framework/moven_moduleTest/NewClassTest.java
@@ -0,0 +1,13 @@
+package com.Framework.moven_moduleTest;
+
+import org.testng.annotations.Test;
+
+public class NewClassTest {
+	
+	@Test
+	public void firstTest() {
+		System.out.println("FtrstFileTo maven");
+		
+	}
+
+}
