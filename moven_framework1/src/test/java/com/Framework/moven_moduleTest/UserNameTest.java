0db948e261f6683a88f65c3441fcda16a53965d9
Ravi <Ravi@Ravi-PC>
2019-07-20 17:57:28 +0530

    UserNameTestIncluded

---
+++ moven_framework1/src/test/java/com/Framework/moven_moduleTest/UserNameTest.java
@@ -0,0 +1,14 @@
+package com.Framework.moven_moduleTest;
+
+import org.testng.annotations.Test;
+
+public class UserNameTest {
+	
+	@Test
+	
+	public void MyNameTest() {
+		
+		System.out.println("My Name is Ravindar");
+	}
+
+}
