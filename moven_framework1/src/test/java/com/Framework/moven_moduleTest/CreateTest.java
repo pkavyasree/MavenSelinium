bae3d611b9b9a7c6225dd8e13e3c732661974e07
Ravi <Ravi@Ravi-PC>
2019-07-29 10:07:32 +0530

    ggg

--- moven_framework1/src/test/java/com/Framework/moven_moduleTest/CreateTest.java
+++ moven_framework1/src/test/java/com/Framework/moven_moduleTest/CreateTest.java
@@ -1,15 +1,24 @@
 package com.Framework.moven_moduleTest;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 public class CreateTest {
 		
 	@Test(groups="SanityTest")
 	public void createcustomer() {
-	System.out.println("createcustomer1 created");	
+	System.out.println("createcustomer15555 created");	
 		
 	}
 	
 	@Test(groups="RegressionTest")
-	public void createcustomerTest() {
-	System.out.println("createcustomer2 created");	
+	public void createcustomeraTest() {
+		
+		String expected="xyz";
+		String Actual= "abc";
+	Reporter.log("Validating createcustomerTest");
+		Assert.assertEquals(Actual, expected);
+			
+		
+	System.out.println("createcustomer2444 created");	
 	}	
 }
