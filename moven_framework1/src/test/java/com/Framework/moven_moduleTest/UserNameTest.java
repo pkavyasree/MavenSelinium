64f8ebee3e2274b34c0e6825ad122d23c7b5c578
Ravi <Ravi@Ravi-PC>
2019-08-22 20:38:09 +0530

    Added validation in UserTest

--- moven_framework1/src/test/java/com/Framework/moven_moduleTest/UserNameTest.java
+++ moven_framework1/src/test/java/com/Framework/moven_moduleTest/UserNameTest.java
@@ -2,13 +2,17 @@ package com.Framework.moven_moduleTest;
 
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 public class UserNameTest {
 	
 	@Test
 	
 	public void MyNameTest() {
+		 Assert.assertEquals("Ravi", "Ravindar");
+		 
 		
-		System.out.println("My Name is Ravindar");
+		
 	}
 
 }
